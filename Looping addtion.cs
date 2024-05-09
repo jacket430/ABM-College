6ddd18892cbcf3e469325d54325d5160fb5545d2
jacket430 <avemitchell@gmail.com>
2024-05-09 17:34:36 -0600

    Added yesterday's practice scripts

---
+++ Looping addtion.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        for(int i=1;i<=10;i++){
+            Console.WriteLine (i);
+        }
+    }
+}
---
+++ Sum of two numbers.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class HelloWorld
+{
+        public static void Main(string[] args)
+        {
+            var a=10;
+            var b=20;
+            var c=a+b;
+            Console.WriteLine ("The sum of A and B is " + c);
+        }
+}
