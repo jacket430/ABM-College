83a240d270569758efbe9edcfdcfe403d2e1d64b
jacket430 <avemitchell@gmail.com>
2024-05-21 21:44:27 -0600

    Create Printing triangle using loops.cs

---
+++ Printing triangle using loops.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int numberOfLines = 6;
+
+        for (int i = 1; i <= numberOfLines; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
