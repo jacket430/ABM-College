77ceb5527275cb2e25420ac85b1571660951b77d
jacket430 <avemitchell@gmail.com>
2024-05-21 21:44:22 -0600

    Create Days between dates.cs

---
+++ Days between dates.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        DateTime firstDate = new DateTime(2023, 1, 1); // <--- Year, Month, Day
+        DateTime secondDate = new DateTime(2022, 1, 2);
+
+        TimeSpan difference = firstDate - secondDate;
+        int daysBetween = Math.Abs(difference.Days);
+
+        Console.WriteLine($"{daysBetween} days");
+    }
+}
