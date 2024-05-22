fd506906ee444b112c7bf8e6a13dd2b226cd7706
jacket430 <avemitchell@gmail.com>
2024-05-21 21:44:29 -0600

    Create Square Root.cs

---
+++ Square Root.cs
@@ -0,0 +1,35 @@
+﻿uusing System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Prime numbers from 1 to 100:");
+
+        for (int number = 2; number <= 100; number++)
+        {
+            if (IsPrime(number))
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
