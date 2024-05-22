99a2381397bfdc42b749a967a64f7849767a29bd
jacket430 <avemitchell@gmail.com>
2024-05-21 21:44:24 -0600

    Create Days since birthday.cs

---
+++ Days since birthday.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        DateTime myBirthday = new DateTime(1997, 2, 1);
+
+        DateTime today = DateTime.Today;
+        TimeSpan age = today - myBirthday;
+        int daysSinceMyBirthday = age.Days;
+
+        DayOfWeek dayofWeek = myBirthday.DayOfWeek;
+
+        Console.WriteLine($"My birthday was {myBirthday.ToString("MMMM d, yyyy")}");
+        Console.WriteLine($"It has been {daysSinceMyBirthday} days since I was born");
+        Console.WriteLine($"It was on a {dayofWeek}");
+    }
+}
