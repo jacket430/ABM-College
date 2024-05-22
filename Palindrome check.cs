d2dc2f6f97fa820b2d7f3fa902cf97b239e0242b
jacket430 <avemitchell@gmail.com>
2024-05-21 21:44:26 -0600

    Create Palindrome check.cs

---
+++ Palindrome check.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Enter a word:");
+		string input = Console.ReadLine();
+
+		char[] charArray = input.ToCharArray();
+		Array.Reverse(charArray);
+		string reversed = new string(charArray);
+
+		bool isPalindrome = input == reversedInput;
+
+		if (isPalindrome)
+		{
+			Console.WriteLine("The input is a palindrome.");
+		}
+		else
+		{
+			Console.WriteLine("The input is not a palindrome.")
+		}
+	}
+}
